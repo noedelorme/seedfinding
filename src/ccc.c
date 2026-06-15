c5cfcfc14d1c2ac769d3f3ef7da63b038dcf762b
noedelorme <lenoedelorme@gmail.com>
2026-06-15 22:59:15 +0200

    start generating seeds

--- src/ccc.c
+++ src/ccc.c
@@ -1,5 +1,7 @@
 // find a seed with a certain structure at the origin chunk
 #include "finders.h"
+#include "loot/loot_table_context.h"
+#include "loot/items.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,9 +12,10 @@ int main()
     Generator g;
     setupGenerator(&g, version, 0);
 
+    int bt_v_dist = 16*3;
+
     uint64_t lower48;
-    for (lower48 = 0; ; lower48++)
-    {
+    for (lower48 = 0; ; lower48++) {
         Pos bt_pos;
         if (!getStructurePos(Treasure, version, lower48, 0, 0, &bt_pos))
             continue;
@@ -21,14 +24,49 @@ int main()
         if (!getStructurePos(Village, version, lower48, 0, 0, &v_pos))
             continue;
 
-        if (abs(bt_pos.x-v_pos.x) >= 16 || abs(bt_pos.z-v_pos.z) >= 16)
+        if (abs(bt_pos.x-v_pos.x) >= bt_v_dist || abs(bt_pos.z-v_pos.z) >= bt_v_dist)
             continue;
+
+        printf("... start testing lower48=%" PRId64 " ...\n", (int64_t) lower48);
 
         uint64_t upper16;
         for (upper16 = 0; upper16 < 0x10000; upper16++)
         {
             uint64_t seed = lower48 | (upper16 << 48);
             applySeed(&g, DIM_OVERWORLD, seed);
+
+            int biome = getBiomeAt(&g, 1, v_pos.x, 63, v_pos.z);
+            if (biome != plains && biome !=taiga)
+                continue;
+
+            StructureSaltConfig salt_config;
+            getStructureSaltConfig(Treasure, version, -1, &salt_config);
+            StructureVariant structure_variant;
+            getVariant(&structure_variant, Treasure, version, seed, bt_pos.x, bt_pos.z, -1);
+            Piece piece;
+            getStructurePieces(&piece, 1, Treasure, salt_config, &structure_variant, version, seed, bt_pos.x, bt_pos.z);
+
+            LootTableContext* ctx;
+            init_loot_table_name(&ctx, piece.lootTables[0], version);
+            set_loot_seed(ctx, piece.lootSeeds[0]);
+            generate_loot(ctx);
+
+            int iron_count = 0;
+            int gold_count = 0;
+            int emerald_count = 0;
+            int tnt_count = 0;
+            for (int i = 0; i < ctx->generated_item_count; i++) {
+                ItemStack item = ctx->generated_items[i];
+                if (ctx->global_item_ids[item.item] == ITEM_IRON_INGOT) { iron_count += item.count; }
+                if (ctx->global_item_ids[item.item] == ITEM_GOLD_INGOT) { gold_count += item.count; }
+                if (ctx->global_item_ids[item.item] == ITEM_EMERALD) { emerald_count += item.count; }
+                if (ctx->global_item_ids[item.item] == ITEM_TNT) { tnt_count += item.count; }
+            }
+
+            int cond = tnt_count >= 1 && emerald_count >= 3 && (iron_count >= 12 || (iron_count >= 10 && gold_count >= 2));
+
+            if (!cond)
+                continue;
 
             Pos spawn_pos = getSpawn(&g);
             if (spawn_pos.x >= 16 || spawn_pos.z >= 16)
@@ -40,13 +78,13 @@ int main()
             if (!isViableStructurePos(Village, &g, v_pos.x, v_pos.z, 0))
                 continue;
 
-            printf("Seed %" PRId64 " has a Treasure at (%d, %d) and a Village (%d, %d).\n", (int64_t) seed, bt_pos.x, bt_pos.z, v_pos.x, v_pos.z);
-            // printf("Seed %" PRId64 " has a Treasure at (%d, %d).\n", (int64_t) seed, bt_pos.x, bt_pos.z);
-
+            // printf("Seed %" PRId64 " bt at (%d, %d) and village at (%d, %d).\n", (int64_t) seed, bt_pos.x, bt_pos.z, v_pos.x, v_pos.z);
+            printf("%" PRId64 "\n", (int64_t) seed);
         }
-
-        printf("------------------------");
     }
 
     return 0;
 }
+
+
+// stdbuf -oL -eL ./build/ccc > seed/ccc.txt 2>&1
--- src/loot.c
+++ src/loot.c
@@ -4,6 +4,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printPiece(Piece *p)
+{
+    printf("name: %s\n", p->name);
+
+    printf("pos: (%d, %d, %d)\n",
+           p->pos.x, p->pos.y, p->pos.z);
+
+    printf("bb0: (%d, %d, %d)\n",
+           p->bb0.x, p->bb0.y, p->bb0.z);
+
+    printf("bb1: (%d, %d, %d)\n",
+           p->bb1.x, p->bb1.y, p->bb1.z);
+
+    printf("rot: %u\n", p->rot);
+    printf("depth: %d\n", p->depth);
+    printf("type: %d\n", p->type);
+
+    printf("chestCount: %d\n", p->chestCount);
+
+    for (int i = 0; i < p->chestCount; i++)
+    {
+        printf("chest %d:\n", i);
+
+        printf("  pos = (%d, %d)\n",
+               p->chestPoses[i].x,
+               p->chestPoses[i].z);
+
+        printf("  lootSeed = %llu\n",
+               (unsigned long long)p->lootSeeds[i]);
+
+        printf("  lootTable = %s\n",
+               p->lootTables[i]);
+    }
+
+    printf("additionalData: %d\n", p->additionalData);
+}
+
 int main()
 {
     int version = MC_1_21;
@@ -17,15 +54,42 @@ int main()
     Pos bt_pos;
     getStructurePos(Treasure, version, seed, 0, 0, &bt_pos);
 
+    StructureSaltConfig salt_config;
+    getStructureSaltConfig(Treasure, version, -1, &salt_config);
+    StructureVariant structure_variant;
+    getVariant(&structure_variant, Treasure, version, seed, bt_pos.x, bt_pos.z, -1);
+    Piece piece;
+    getStructurePieces(&piece, 1, Treasure, salt_config, &structure_variant, version, seed, bt_pos.x, bt_pos.z);
+
     LootTableContext* ctx;
-    init_buried_treasure(&ctx, version);
-    set_loot_seed(ctx, 2); // it reamins to understand how to compute the lootseed as a function of worldseed and pos of bt
+    init_loot_table_name(&ctx, piece.lootTables[0], version);
+    set_loot_seed(ctx, piece.lootSeeds[0]);
     generate_loot(ctx);
 
+    int iron_count = 0;
+    int gold_count = 0;
+    int emerald_count = 0;
+    int tnt_count = 0;
     for (int i = 0; i < ctx->generated_item_count; i++) {
         ItemStack item = ctx->generated_items[i];
-        printf("%d x %s\n", item.count,ctx->item_names[item.item]);
+        if (ctx->global_item_ids[item.item] == ITEM_IRON_INGOT) {
+            iron_count += item.count;
+        }
+        if (ctx->global_item_ids[item.item] == ITEM_GOLD_INGOT) {
+            gold_count += item.count;
+        }
+        if (ctx->global_item_ids[item.item] == ITEM_EMERALD) {
+            emerald_count += item.count;
+        }
+        if (ctx->global_item_ids[item.item] == ITEM_TNT) {
+            tnt_count += item.count;
+        }
     }
+
+    printf("iron: %d\n", iron_count);
+    printf("gold: %d\n", gold_count);
+    printf("emerald: %d\n", emerald_count);
+    printf("tnt: %d\n", tnt_count);
 
     return 0;
 }
